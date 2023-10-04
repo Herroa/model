95897acb7fa589d968f769169a75dd103aa57a38
Herroa <roman.gordov@mail.ru>
2023-10-04 19:29:10 +0700

    NR to fix

--- RFmodel/main.cpp
+++ RFmodel/main.cpp
@@ -14,32 +14,36 @@ public:
     Animal(int x, int y, int stability, int direction) : x(x), y(y), stability(stability), direction(direction), age(0)
     {
     }
-    void move(int max_n,int max_m)
+    void move(int max_n, int max_m)
     {
         switch (direction)
         {
         case 0:
             y--;
-            if(y<0){
-                y = max_n-1;
+            if (y < 0)
+            {
+                y = max_n - 1;
             }
             break;
         case 1:
             x++;
-            if(y==max_m){
+            if (y == max_m)
+            {
                 x = 0;
             }
             break;
         case 2:
             y++;
-            if(y==max_n){
+            if (y == max_n)
+            {
                 y = 0;
             }
             break;
         case 3:
             x--;
-            if(x<0){
-                x = max_m-1;
+            if (x < 0)
+            {
+                x = max_m - 1;
             }
             break;
         }
@@ -54,10 +58,10 @@ public:
     {
         age++;
     }
-    int get_x(){return x;}
-    int get_y(){return y;}
-    int get_stability(){return stability;}
-    int get_age(){return age;}
+    int get_x() { return x; }
+    int get_y() { return y; }
+    int get_stability() { return stability; }
+    int get_age() { return age; }
 };
 
 class Rabbit : public Animal
@@ -66,10 +70,10 @@ private:
     int breed;
 
 public:
-    Rabbit(int x, int y, int stability, int direction) : Animal(x, y, stability, direction), breed(25)
+    Rabbit(int x, int y, int stability, int direction) : Animal(x, y, stability, direction), breed(10)
     {
     }
-    int get_breed(){return breed;}
+    int get_breed() { return breed; }
 };
 
 class Fox : public Animal
@@ -102,51 +106,59 @@ public:
             mas[i] = new int[m];
         }
     }
-    void step(int i){
-        //rabbit
+    void step(int i)
+    {
+        // rabbit
         unsigned NR = masR.size();
-        for(unsigned r = 0;r<NR;r++){
-            if(i%masR[r].get_breed()==0){
-                addR(masR[r].get_x(),masR[r].get_y(),masR[r].get_stability(),masR[r].get_age());
-            }
-            if(masR[r].get_age()==50){
-               masR.erase(masR.begin()+r); 
-            }
-            if(i%masR[r].get_stability()==0){
+        for (unsigned r = 0; r < NR; r++)
+        {
+            masR[r].move(n, m);
+            masR[r].age1();
+            if (i % masR[r].get_stability() == 0)
+            {
                 masR[r].changeD();
             }
-            masR[r].move(n,m);
-            masR[r].age1();
-            
+            if (masR[r].get_age() == 50)
+            {
+                masR.erase(masR.begin() + r);
+            }
+            if (i % masR[r].get_breed() == 1)
+            {
+                addR(masR[r].get_x()+1, masR[r].get_y()+1, masR[r].get_stability(), masR[r].get_age());
+            }
         }
     };
     void draw(int i)
     {
-        std:: cout << "Model step: " << i << std::endl;
+        std::cout << "Model step: " << i << std::endl;
         for (int i = 0; i < n; i++)
         {
             for (int j = 0; j < m; j++)
             {
                 int flag = 0;
-                for(unsigned r = 0;r<masR.size();r++){
-                    if((masR[r].get_x() == j) && (masR[r].get_y() == i)){
+                for (unsigned r = 0; r < masR.size(); r++)
+                {
+                    if ((masR[r].get_x() == j) && (masR[r].get_y() == i))
+                    {
                         std::cout << 'R';
                         flag = 1;
                         break;
                     }
                 }
-                for(unsigned r = 0;r<masF.size();r++){
-                    if((masF[r].get_x() == j) && (masF[r].get_y() == i)){
+                for (unsigned r = 0; r < masF.size(); r++)
+                {
+                    if ((masF[r].get_x() == j) && (masF[r].get_y() == i))
+                    {
                         std::cout << 'F';
                         flag = 1;
                         break;
                     }
                 }
-                //проверять существует ли заяц или лиса на этой координате 'F' 'R'
-                if(!flag){
+                // проверять существует ли заяц или лиса на этой координате 'F' 'R'
+                if (!flag)
+                {
                     std::cout << '_';
                 }
-                
             }
             std::cout << std::endl;
         }
@@ -171,7 +183,7 @@ int main()
     // M.addF(7, 7, 5, 0);
     // Ввод данных для лис и добавление лис (через объект класс Модель)
     // ХОД
-    int K = 1000;
+    int K = 50;
     for (int i = 0; i < K; ++i)
     {
         M.draw(i);
